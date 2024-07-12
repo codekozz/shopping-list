06c2b0912483690bf53ff57d1ae90251464adf97
Nastiky92 <nastyaralchik@gmail.com>
2024-07-12 19:33:59 +0300

    Обновлен файл js
    добавлен скрипт

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-/* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('#input');
+const itemsContiner = document.querySelector('.items');
 
-/* Пустые элементы не должны добавляться */
+function sendItem(event) {
 
-/* Если кликнуть на элемент списка, он зачеркивается */
+    if (event.key == 'Enter') {
+        const itemText = sendInput.value;
+        const newItem = document.createElement('div');
 
-/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
+        newItem.textContent = itemText;
 
-/* Очищать input после добавления нового элемента в список */
+        if (itemText != '') {
+            itemsContiner.append(newItem);
+        }
+
+        sendInput.value = '';
+
+        newItem.addEventListener('click', function() {
+
+            newItem.classList.toggle('done');
+
+        });
+    }
+}
+
+sendInput.addEventListener('keydown', sendItem);
